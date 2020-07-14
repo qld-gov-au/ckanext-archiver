12515647a550f2034535fac379a87ed68830c58a
william dutton <will.dutt@gmail.com>
2020-07-14 11:48:55 +1000

    show logs on failure

--- bin/travis-run.sh
+++ bin/travis-run.sh
@@ -1,4 +1,5 @@
 #!/bin/sh -e
 
 
-nosetests --nologcapture --with-pylons=subdir/test-core.ini --with-coverage --cover-package=ckanext.archiver --cover-inclusive --cover-erase --cover-tests
+#nosetests --nologcapture --with-pylons=subdir/test-core.ini --with-coverage --cover-package=ckanext.archiver --cover-inclusive --cover-erase --cover-tests
+nosetests --with-pylons=subdir/test-core.ini --with-coverage --cover-package=ckanext.archiver --cover-inclusive --cover-erase --cover-tests
